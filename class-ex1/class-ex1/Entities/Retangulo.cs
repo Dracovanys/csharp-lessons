774c1a40654752c8443627a90882b45cbcef2bb5
Mateus Coelho <mat_de_souza@hotmail.com>
2021-06-19 21:16:45 -0400

    Adding class-ex1

---
+++ class-ex1/class-ex1/Entities/Retangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Entities
+{
+	class Retangulo
+	{
+		public double largura { get; set; }
+		public double altura { get; set; }
+
+		public Retangulo(double altura, double largura)
+		{
+			this.largura = largura;
+			this.altura = altura;
+		}
+
+		public override string ToString()
+		{
+			return "Altura: " + altura + ", Largura: " + largura;
+		}
+
+		public double Area()
+		{
+			return altura * largura;
+		}
+		public double Perimetro()
+		{
+			return (largura + altura) * 2;
+		}
+
+		public double Diagonal()
+		{
+			return Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+		}
+
+	}
+}
---
+++ class-ex1/class-ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Entities;
+
+namespace class_ex1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Entre com a altura e largura do retangulo:");
+			double altura = double.Parse(Console.ReadLine());
+			double largura = double.Parse(Console.ReadLine());
+
+			Retangulo ret = new Retangulo(altura, largura);
+
+			Console.WriteLine("AREA = " + ret.Area().ToString("F2") 
+				+ "\nPERÍMETRO = " + ret.Perimetro().ToString("F2")
+				+ "\nDIAGONAL = " +  ret.Diagonal().ToString("F2"));
+
+			Console.ReadLine();
+		}
+	}
+}

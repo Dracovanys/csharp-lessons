5f23366f59ff651596f2cc53f5ed13b8b783db83
Mateus Coelho <mat_de_souza@hotmail.com>
2021-06-19 21:30:19 -0400

    Adding static-members

---
+++ static-members/static-members/Entities/ConversorDeMoedas.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities
+{
+	static class ConversorDeMoedas
+	{
+		public static double Dolar(double cota, double valor)
+		{
+			return (valor * cota) + (0.06 * (valor * cota));
+		}
+	}
+}
---
+++ static-members/static-members/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Entities;
+
+namespace static_members
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Qual é a cotação do dólar? ");
+			double cota = double.Parse(Console.ReadLine());
+			Console.Write("Quantos dólares você vai comprar? ");
+			double valor = double.Parse(Console.ReadLine());
+
+			Console.WriteLine("Valor a ser pago em reais = " + ConversorDeMoedas.Dolar(cota, valor).ToString("F2"));
+
+			Console.ReadLine();
+		}
+	}
+}

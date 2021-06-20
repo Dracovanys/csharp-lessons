926e345c367f1c6ede94c6ffadaf75f26c4d4455
Mateus Coelho <mat_de_souza@hotmail.com>
2021-06-19 21:57:43 -0400

    Adding constructor

---
+++ constructor/constructor/Entities/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Entities
+{
+	class ContaBancaria
+	{
+		public int numConta { get; private set; }
+		public string nomeTitular { get; set; }
+		public double depositoInicial { get; set; }
+		public double saldo { get; private set; }
+
+		public ContaBancaria()
+		{
+		}
+
+		public ContaBancaria(int numConta, string nomeTitular, double depositoInicial)
+		{
+			this.numConta = numConta;
+			this.nomeTitular = nomeTitular;
+			this.depositoInicial = depositoInicial;
+			this.saldo = depositoInicial;
+
+		}
+
+		public ContaBancaria(int numConta, string nomeTitular)
+		{
+			this.numConta = numConta;
+			this.nomeTitular = nomeTitular;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			sb.Append("\nINFORMAÇÕES DA CONTA");
+			sb.Append("\nNúmero: " + numConta);
+			sb.Append("\nTitular: " + nomeTitular);
+			sb.Append("\nSaldo: $" + saldo.ToString("F2") + "\n");
+
+			return sb.ToString();
+		}
+
+		public void Deposito(double valor)
+		{
+			saldo += valor;
+			System.Console.WriteLine(this);
+		}
+
+		public void Saque(double valor)
+		{
+			saldo -= valor + 5;
+			System.Console.WriteLine(this);
+		}
+	}
+}
---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Entities;
+
+namespace constructor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ContaBancaria conta = new ContaBancaria();
+
+			Console.Write("Entre o número da conta: ");
+			int numConta = int.Parse(Console.ReadLine());
+			Console.Write("Entre o titular da conta: ");
+			string titular = Console.ReadLine();
+			Console.Write("Haverá depósito inicial(s/n)? ");
+			char op = char.Parse(Console.ReadLine());
+
+			if (op == 's')
+			{
+				Console.Write("Entre o valor do depósito inicial: ");
+				double depos = double.Parse(Console.ReadLine());
+
+				conta = new ContaBancaria(numConta, titular, depos);
+			} else if (op == 'n')
+			{
+				conta = new ContaBancaria(numConta, titular);
+			}
+
+			Console.WriteLine(conta);
+
+			Console.Write("Entre um valor para depósito: ");
+			double dep = double.Parse(Console.ReadLine());
+			conta.Deposito(dep);
+
+			Console.Write("Entre um valor para saque: ");
+			double saque = double.Parse(Console.ReadLine());
+			conta.Saque(saque);
+
+			Console.ReadLine();
+
+
+		}
+	}
+}
